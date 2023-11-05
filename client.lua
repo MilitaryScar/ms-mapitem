9d36cc13cdfdf2f9b9c79536a250f97937c70310
MilitaryScar <99544566+MilitaryScar@users.noreply.github.com>
2023-11-05 15:59:25 +0000

    Update client.lua - Added emote support

--- client.lua
+++ client.lua
@@ -1,7 +1,39 @@
 QBCore = exports['qb-core']:GetCoreObject()
+local open = false
 
 RegisterNetEvent('ms:client:openmap', function()
-ActivateFrontendMenu(GetHashKey('FE_MENU_VERSION_MP_PAUSE'),0,-1) 
-SetNuiFocus(true, true)
-open = false
+    if not open then
+        ActivateFrontendMenu(GetHashKey('FE_MENU_VERSION_MP_PAUSE'), 0, -1) 
+        SetNuiFocus(true, true)
+        open = true
+        TriggerEvent('animations:client:EmoteCommandStart', {"map2"})
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if open and IsControlPressed(1, 202) then
+            TriggerEvent('animations:client:EmoteCommandStart', {"c"})
+            open = false
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    Citizen.Wait(100)
+
+    while true do
+        local sleepThread = 500
+
+        local radarEnabled = IsRadarEnabled()
+
+        if not IsPedInAnyVehicle(PlayerPedId()) and radarEnabled then
+            DisplayRadar(false)
+        elseif IsPedInAnyVehicle(PlayerPedId()) and not radarEnabled then
+            DisplayRadar(false)
+        end
+
+        Citizen.Wait(sleepThread)
+    end
 end)
